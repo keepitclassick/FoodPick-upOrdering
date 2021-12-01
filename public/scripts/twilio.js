9f7e573ea7800bb0f9b0173f2cafffa9163e35f5
keepitclassick <keepitclassick@gmail.com>
2021-12-01 10:51:55 -0500

    added functions to send messages for each stage of order

--- public/scripts/twilio.js
+++ public/scripts/twilio.js
@@ -1,11 +1,10 @@
 require('dotenv').config();
 
-//  Responsible of sending SMS
+//  Responsible for sending SMS
 const { endianness } = require('os');
 
 const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
-const toPhone = process.env.TO_PHONE;
 const client = require('twilio')(accountSid, authToken);
 const user = {
   userName: 'Old Man Jenkins',
@@ -14,10 +13,40 @@ const user = {
   cost: '$20'
 }
 
-client.messages
-  .create({
-     body: `Hello ${user.userName}, thank you for your order! The total is ${user.cost}. Please pick up your order in ${user.pickUpTime} minutes.`,
-     from: process.env.TWILIO_PHONE,
-     to: process.env.TO_PHONE
-  })
-  .then(message => console.log(message.sid));
+  const orderConfirmed = function (user) {
+
+    client.messages
+      .create({
+        body: `Hello ${user.userName}, thank you for your order! The total is ${user.cost}.`,
+        from: process.env.TWILIO_PHONE,
+        to: process.env.TO_PHONE
+      })
+      .then(message => console.log(message.sid));
+  }
+
+  const timeConfirmed = function (time_est) {
+    client.messages
+    .create({
+      body: `Your order will be ready in approximately ${time_est} minutes.`,
+      from: process.env.TWILIO_PHONE,
+      to: process.env.TO_PHONE
+    })
+    .then(message => console.log(message.sid));
+  }
+
+  const orderCompleted = function () {
+    client.messages
+    .create({
+      body: `Your order is completed! 😊`,
+      from: process.env.TWILIO_PHONE,
+      to: process.env.TO_PHONE
+    })
+    .then(message => console.log(message.sid));
+  }
+
+
+  orderConfirmed(user);
+  timeConfirmed(5);
+  orderCompleted();
+
+  module.exports = { orderConfirmed, timeConfirmed, orderCompleted };
