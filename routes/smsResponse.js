ce8015b987b4b0e8b7f55b14b9b0cf24fe8d4661
Stefan Darrell Davis <stefan.davis@mail.utoronto.ca>
2021-12-01 20:22:06 +0000

    added vendors section to nav bar and working on getting twillo up

--- routes/smsResponse.js
+++ routes/smsResponse.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const MessagingResponse = require('twilio/lib/twiml/MessagingResponse');
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
 const router  = express.Router();
 
+router.use(function(req, res, next) {
+  console.log('Time', Date.now());
+  next ();
+});
 
 module.exports = (db) => {
   app.post("/sms", (req, res) => {
     const twiml = new MessagingResponse();
-
-    twiml.message('Your order is confirmed!')
-
-    res.writeHead(200, { 'Content-Type': 'text/xml'})
+    twiml.message('Your order is confirmed!');
+    res.writeHead(200, { 'Content-Type': 'text/xml'});
     res.end(twiml.toString());
   });
 };
