673e948047667262db488578c1b7f1566bbcaa9a
keepitclassick <keepitclassick@gmail.com>
2021-12-02 09:43:33 -0500

    admin route chart fixed, admin nav bar update to go home and to specific ids

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -107,38 +107,3 @@ function updateCartTotal() {
     document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
 }
 
-function createTable(objectArray, fields, fieldTitles) {
-  let body = document.getElementsByTagName('body')[0];
-  let tbl = document.createElement('table');
-  let thead = document.createElement('thead');
-  let thr = document.createElement('tr');
-  fieldTitles.forEach((fieldTitle) => {
-    let th = document.createElement('th');
-    th.appendChild(document.createTextNode(fieldTitle));
-    thr.appendChild(th);
-  });
-  thead.appendChild(thr);
-  tbl.appendChild(thead);
-
-  let tbdy = document.createElement('tbody');
-  let tr = document.createElement('tr');
-  objectArray.forEach((object) => {
-    let tr = document.createElement('tr');
-    fields.forEach((field) => {
-      var td = document.createElement('td');
-      td.appendChild(document.createTextNode(object[field]));
-      tr.appendChild(td);
-    });
-    tbdy.appendChild(tr);
-  });
-  tbl.appendChild(tbdy);
-  body.appendChild(tbl)
-  return tbl;
-}
-
-createTable([
-  {name: 'Banana', price: '3.04'},
-  {name: 'Orange', price: '2.56'},
-  {name: 'Apple', price: '1.45'}
-],
-['name', 'price'], ['Name', 'Price']);
--- server.js
+++ server.js
@@ -36,13 +36,11 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
 const admin = require("./routes/admin");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
-app.use("/api/admin", widgetsRoutes(db));
 app.use("/admin", admin(db));
 // Note: mount other resources here, using the same pattern above
 
