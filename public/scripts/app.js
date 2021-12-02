b49f79f724c7d81213275a3c5d4ebccc155b135e
keepitclassick <keepitclassick@gmail.com>
2021-12-02 15:58:07 -0500

    second event listener added to admin

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', init, false);
 
-
 function init(){
   function message () {
     alert("Hello!");
   }
   let button = document.getElementById('confirm');
   button.addEventListener('click', message, true);
+
+  let button2 = document.getElementById('complete');
+  button2.addEventListener('click', message, true);
+
 };
