ae2c416bfe96f949136187a51d58b83f03a554ec
Stefan Darrell Davis <60591525+Davisstef@users.noreply.github.com>
2021-12-02 22:36:14 -0500

    Update app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,11 +5,10 @@ document.addEventListener('DOMContentLoaded', init, false);
 
 function init(){
   function message () {
-    alert("Hello!");
   }
   let button1 = document.getElementById('confirm');
   button1.addEventListener('click', message, true);
   let button2 = document.getElementById('complete');
   button2.addEventListener('click', message, true);
-  console.log(message.sid)
+  console.log('message.sid')
 };
